5312fdcb6cc363814f11c490c43ffb1579f426d6
sue991 <sue991@naver.com>
2021-01-01 14:46:37 +0900

    201231 SCS(Simple Client and Server)

---
+++ Testclient/Testclient/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Testclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(340, 22);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(80, 23);
+            this.btn_connect.TabIndex = 0;
+            this.btn_connect.Text = "connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 75);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(293, 54);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Enabled = false;
+            this.btn_send.Location = new System.Drawing.Point(340, 108);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(80, 21);
+            this.btn_send.TabIndex = 2;
+            this.btn_send.Text = "Send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(24, 148);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(395, 160);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(66, 22);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(100, 21);
+            this.txtHost.TabIndex = 5;
+            this.txtHost.TextChanged += new System.EventHandler(this.txtHost_TextChanged);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(261, 22);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(56, 21);
+            this.txtPort.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Host :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Port : ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Message";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 331);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_connect);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Testclient/Testclient/Form1.cs
@@ -0,0 +1,90 @@
+﻿using Hik.Communication.Scs.Client;
+using Hik.Communication.Scs.Communication.EndPoints.Tcp;
+using Hik.Communication.Scs.Communication.Messages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testclient
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        IScsClient client;
+
+        private void btn_connect_Click(object sender, EventArgs e) {
+            btn_connect.Enabled = false;
+
+            client = ScsClientFactory.CreateClient(new ScsTcpEndPoint(txtHost.Text, Convert.ToInt32(txtPort.Text)));
+            client.MessageReceived += Client_MessageReceived;
+
+            client.Connected += Client_Connected;
+            client.Connect();
+
+           
+        }
+
+        private void Client_Connected(object sender, EventArgs e)
+        {
+            Console.WriteLine("connected");
+            btn_send.Enabled = true;
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            var messageText = textBox1.Text; 
+            client.SendMessage(new ScsTextMessage(messageText));
+
+             
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtHost.Text = "127.0.0.1";
+            txtPort.Text = "10085";
+
+        }
+
+        private void Client_MessageReceived(object sender, MessageEventArgs e)
+        {
+            var message = e.Message as ScsTextMessage;
+            if (message == null)
+            {
+                return;
+            }
+            Console.WriteLine("Server sent a message: " + message.Text);
+
+            textBox2.Invoke((MethodInvoker)delegate ()
+            {
+                textBox2.Text += "server : " + message.Text + '\n';
+
+            });
+        }
+
+        private void txtHost_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
