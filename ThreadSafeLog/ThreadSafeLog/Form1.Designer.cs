d4ce33557e6db046a4dbbbc8d258438950513d25
sue991 <sue991@naver.com>
2021-02-09 17:37:47 +0900

    mlultiThread를 다루기 위한 queue

---
+++ ThreadSafeLog/ThreadSafeLog/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace ThreadSafeLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thread Count";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(176, 21);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(153, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "use BlockingCollection";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(33, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(430, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBoxLog
+            // 
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.ItemHeight = 12;
+            this.listBoxLog.Location = new System.Drawing.Point(34, 62);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(471, 352);
+            this.listBoxLog.TabIndex = 6;
+            this.listBoxLog.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 450);
+            this.Controls.Add(this.listBoxLog);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "`";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBoxLog;
+    }
+}
+
---
+++ ThreadSafeLog/ThreadSafeLog/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ThreadSafeLog
+{
+    public partial class Form1 : Form
+    {
+        private static ConcurrentQueue<Log> q = new ConcurrentQueue<Log>();
+        private static int num = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                int threadCount = Convert.ToInt32(textBox1.Text);
+                Thread[] t = new Thread[threadCount];
+                for (int i = 0; i < threadCount; i++)
+                {
+                    t[i] = new Thread(threadStart);
+                    t[i].Start();
+                }
+
+
+            }
+
+            Thread th = new Thread(Run);
+            th.Start();
+           
+        }
+
+        private void updateUI(string s)
+        {
+            Func<int> del = delegate ()
+            {
+                if (listBoxLog.Items.Count > 200)
+                {
+                    listBoxLog.Items.RemoveAt(listBoxLog.Items.Count - 1);
+                }
+                listBoxLog.Items.Insert(0, s);
+                return 0;
+            };
+            Invoke(del);
+        }
+        private void Run()
+        {
+            while (true)
+            {
+                if (!q.IsEmpty)
+                {
+                    var n = q.TryDequeue(out var item); // Race condition!
+                    updateUI(item.Msg);
+                    // Process(item);
+                }
+                else
+                {
+                    Thread.Sleep(50);
+                }
+            }
+        }
+        class Log
+        {
+            public string File { get; set; }
+            public string Msg { get; set; }
+        }
+        public static void addLog(string logFile, string msg)
+        {
+            Log log = new Log()
+            {
+                File = logFile,
+                Msg = msg
+            };
+
+            q.Enqueue(log);
+        }
+
+        private static void threadStart()
+        {
+            string msg = "Start" + num.ToString();
+            num += 1;
+            addLog("Logfile",msg);
+
+        }
+
+
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
