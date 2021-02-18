46fe8001db6de353a60f4f6477225931d19d0a82
sue991 <sue991@naver.com>
2021-02-18 23:18:23 +0900

    User Control 을 만들어 사용하기. Property 직접 만들어 사용하기

---
+++ Digital Clock/Digital Clock/UserControl1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; 
+
+namespace Digital_Clock
+{
+    public partial class UserControl1 : UserControl
+    {
+      
+        public enum Dateformats
+        {
+            date12,
+            date24
+        }
+
+        private Dateformats DateStyle;
+
+        [Category("Dateformats"), Description("시간표현방식 설정"), Browsable(true)]
+        public Dateformats Dateformat
+        {
+            get
+            {
+                return DateStyle;
+            }
+            set
+            {
+                DateStyle = value;
+            }
+        }
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (DateStyle == Dateformats.date12)
+            {
+                label1.Text = DateTime.Now.ToString("hh:mm");
+            }
+            else
+            {
+                label1.Text = DateTime.Now.ToString("HH:mm");
+            }
+                label2.Text = DateTime.Now.ToString("ss");
+                label3.Text = DateTime.Now.ToString("MM dd yyyy");
+                label4.Text = DateTime.Now.ToString("dddd");
+            
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userControl21_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ clock/clock/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.userControl11 = new Digital_Clock.UserControl1();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // userControl11
+            // 
+            this.userControl11.AllowDrop = true;
+            this.userControl11.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.userControl11.Dateformat = Digital_Clock.UserControl1.Dateformats.date24;
+            this.userControl11.ForeColor = System.Drawing.SystemColors.Control;
+            this.userControl11.Location = new System.Drawing.Point(37, 28);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(516, 308);
+            this.userControl11.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1048, 559);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private Digital_Clock.UserControl1 userControl11;
+    }
+}
+
